b6e752afa6d3eeb5890e346c74749a00e0ef9a89
averman <averman@hotmail.com>
2020-03-27 15:23:28 +0300

    Задание 3 урок 1

---
+++ 3 task.py
@@ -0,0 +1,6 @@
+digital = input('Введите число :')
+number = int(digital)
+number_1 = int(digital + digital)
+number_2 = int(digital + digital + digital)
+sum = number + number_1 +number_2
+print(sum)
